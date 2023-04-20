d17ae10b706b8b9052554d73443117eb8f7e9133
Pavan Kumar <pavanrnri1818@gmail.com>
2023-04-20 20:35:12 +0530

    this is second commit because avatar is also come from api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Card from './Card'
-// import Avatar from "./Avatar"
-// data function
-function createCard(contact){
-    return <Card 
-    id={contact.id}
-    // key={contact.id}
-    name={contact.name} 
-    email={contact.email}
-    phone={contact.company.name}
-    website={contact.website}
-    address={contact.address.street}
-    /> 
-}
-// Avatar function
-
 
 function App() {
   const [data, setData] = useState([]);
-//   const [avatarData, setAvatarData] = useState([]);
-
-
 
 // 1st API call
   useEffect(() => {
@@ -33,38 +15,26 @@ function App() {
         console.log(error);
       });
   }, []);
-//   2nd API call
-// useEffect(() => {
-//     axios.get('https://avatars.dicebear.com/v2/avataaars/satwik.svg')
-//       .then(response => {
-//         setAvatarData(response.data);
-//         console.log(avatarData)
-//       })
-//       .catch(error => {
-//         console.log(error);
-//       });
-//   }, []);
-  
-// return method
+// avatar imagr
+  const getAvatarURL = (username) => {
+    return `https://avatars.dicebear.com/v2/avataaars/${username}.svg?options[mood][]=happy`;
+  };
   return (
     <div>
-      <h1>API Data:</h1>
-      {/* <ul> */}
-      {data.map(createCard)}
-        {/* {data.map(item => ( */}
-           {/* // <h1>{item.name}</h1>
-        //   <li key={item.id}>
-        //     {item.name}
-        //     <h3>{item.email}</h3>
-        //     <h3>{item.phone}</h3>
-        //     <h3>{item.company.name}</h3>
-        //     <h3>{item.website}</h3>
-        //     <h3>{item.address.street}</h3>
-        //   </li> */}
-        {/* ))} */}
-      {/* </ul> */}
-    </div>
+    {data.map(user => (
+      <div key={user.id}>
+        <Card 
+          img={getAvatarURL(user.name)}
+          name={user.name} 
+          email={user.email}
+          phone={user.phone}
+          company={user.company.name}
+          website={user.website}
+          address={user.address.street}
+        />
+      </div>
+    ))}
+  </div>
   );
 }
-
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,10 @@ function Card (props) {
     return (
         <div class="top">
         <div class="avatar_left">
-            <img src="https://images.pexels.com/photos/5774804/pexels-photo-5774804.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Avatar-img" />
+            <img 
+             src={props.img}
+             alt="Avatar-img"
+            /> 
         </div>
         <div class="heading">
             <h1>{props.name}</h1>
